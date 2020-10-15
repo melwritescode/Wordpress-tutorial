1ec0e5c216d3b52154668e18521ed32e53a03283
Melanie Williams <melaniep518@gmail.com>
2020-10-15 16:46:00 -0600

    Import HTML template, start converting to WP theme -Add html and styling to header, footer, index -Inject WP code to add functionality to pages

--- functions.php
+++ functions.php
@@ -9,7 +9,16 @@
 
 <?php 
   function get_uni_files() {
+    wp_enqueue_script('main-uni-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('uni_main_styles', get_stylesheet_uri());
   }
   add_action('wp_enqueue_scripts', 'get_uni_files');
+
+  function get_tab_titles() {
+    add_theme_support('title-tag');
+  }
+
+  add_action('after_setup_theme', 'get_tab_titles');
 ?>
